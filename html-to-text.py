a5db435a0b1adaa3be0fa1784f46418f381d3851
Ehud Adler <easports96@gmail.com>
2018-08-17 13:01:37 -0400

    Setting up auto-sentiment analysis

--- html-to-text.py
+++
@@ -1,8 +0,0 @@
-import sys
-from bs4 import BeautifulSoup
-
-with open(sys.argv[1]) as markup:
-    soup = BeautifulSoup(markup.read())
-
-with open("example.txt", "w") as f: 
-    f.write(soup.get_text())
